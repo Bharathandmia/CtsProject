620881531a2403eaf64e662705b0a5186bfea60b
bharath <bharathjava001@gmail.com>
2020-02-19 15:15:48 +0530

    First commit..............

---
+++ src/com/git/ex/GitEx.java
@@ -0,0 +1,20 @@
+package com.git.ex;
+
+public class GitEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="Yes";
+		if(hungry=="Yes")
+		{
+			System.out.println("go and eat");
+		}
+		
+		else
+		{
+			System.out.println("fuck off");
+		}
+
+	}
+
+}
